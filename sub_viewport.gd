4a6a4a408dd4af39b938444444929e8e6a90f24e
Guilhem Duval <guilhem.duval@live.fr>
2024-10-20 11:25:22 -0400

    Chess minigame

--- sub_viewport.gd
+++ sub_viewport.gd
@@ -2,11 +2,12 @@ extends SubViewport
 
 var minigame_balloon = preload("res://minigames/balloon_popper/balloon_popper.tscn")
 var minigame_ski = preload("res://minigames/collision/collision.tscn")
+var minigame_physics = preload("res://minigames/physics/physics.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-	var minigame = minigame_balloon.instantiate()
+	var minigame = minigame_physics.instantiate()
 	add_child(minigame)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
