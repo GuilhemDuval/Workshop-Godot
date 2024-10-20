b72264d689017d0ef39eb1069db5212ba4e437c1
Guilhem Duval <guilhem.duval@live.fr>
2024-10-20 18:11:32 -0400

    Load minigame and music

--- main_scene.gd
+++ main_scene.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
 const Ballon = preload("res://balloon.tscn")
+var music_player : AudioStreamPlayer
 
 @export var dialogue_resource : DialogueResource
 @export var dialogue_start : String = "start"
 @onready var duck : AnimatedSprite2D = get_node("PaintBox/DuckSprite")
 @onready var media_box : TextureRect = get_node("MediaBox")
+@onready var minigame : SubViewport = get_node("MinigameScreen/SubViewportContainer/SubViewport")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	music_player = get_node("007Synthwave421k")
+	music_player.play()
 	var ballon: Node = Ballon.instantiate()
 	get_tree().current_scene.add_child(ballon)
 	ballon.start(dialogue_resource, dialogue_start)
--- sub_viewport.gd
+++ sub_viewport.gd
@@ -5,12 +5,31 @@ var minigame_ski = preload("res://minigames/collision/collision.tscn")
 var minigame_chess = preload("res://minigames/physics/physics.tscn")
 var minigame_coin = preload("res://minigames/coin_collector/coin_collector_scene.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-	var minigame = minigame_coin.instantiate()
-	add_child(minigame)
+var current_minigame : Node = null  # Référence au jeu en cours
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready() -> void:
+	load_minigame(0)  # Charge par défaut le minigame avec ID 3 (coin_collector)
+
+# Fonction pour changer le minigame selon l'ID
+func load_minigame(minigame_id: int) -> void:
+	# Supprimer le minijeu actuel s'il y en a un
+	if current_minigame != null:
+		remove_child(current_minigame)
+		current_minigame.queue_free()
+		current_minigame = null
+
+	# Instancier et ajouter le minigame selon l'ID donné
+	match minigame_id:
+		0:
+			current_minigame = minigame_balloon.instantiate()
+		1:
+			current_minigame = minigame_ski.instantiate()
+		2:
+			current_minigame = minigame_chess.instantiate()
+		3:
+			current_minigame = minigame_coin.instantiate()
+		_:
+			print("Invalid minigame ID")
+
+	if current_minigame != null:
+		add_child(current_minigame)
